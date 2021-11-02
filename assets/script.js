1a8d0f5061427451de1e234f9d59fb17a1b2cbdb
Reza Dehghani <75562501+reza-dehghani@users.noreply.github.com>
2021-11-02 18:11:20 +0330

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+var d = document.querySelector("#d");
+var h = document.querySelector("#h");
+var m = document.querySelector("#m");
+var s = document.querySelector("#s");
+
+//set TIME
+let end = new Date("Apr 8, 2021 21:59:59").getTime();
+
+var x = setInterval(function(){
+    // Get today's date and time
+let start = new Date().getTime();
+  // Find the distance between now and the count down date
+  var distance = end - start;
+let eday = Math.floor(distance/(1000 * 60 * 60 * 24));
+let ehour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let eminute = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+let eseconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+d.innerHTML = eday + " روز";
+h.innerHTML = ehour + " ساعت";
+m.innerHTML = eminute + " دقیقه";
+s.innerHTML = eseconds + " ثانیه";
+
+ // If the count down is finished, write some text
+ if (distance < 0) {
+  clearInterval(x);
+  d.innerHTML = "0";
+  h.innerHTML = "0";
+  m.innerHTML = "0";
+  s.innerHTML = "0";
+}
+}, 1000);
+
+var darsad = document.querySelector("#inhundered");
+var percent = document.querySelector("progress");
+darsad.append(" " + percent.getAttribute("value") + " %");
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HiReza.ir</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/style.css" type="text/css"/>
+  <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+</head>
+
+<body>
+<div class="container pt-5">
+    <h1 id="head" class="">سلام؛ خیلی خوش اومدین، در حال طراحی سایت هستم!</h1>
+        <p>اگه می خوای موقع راه اندازی سایت خبرت کنیم، خوشحال میشیم ایمیلت رو این پایین وارد کنی</p>
+        <form action="/">
+        <input type="email" placeholder="ایمیلت رو اینجا وارد کن">
+        <input type="submit" value="به من خبر بده!">
+        </form>
+
+
+        <!-- counter -->
+        <p class="countertext mt-5" id="d"></p>
+        <p class="countertext" id="h"></p>
+        <p class="countertext" id="m"></p>
+        <p class="countertext" id="s"></p>
+        <!-- end counter -->
+        <!-- progress bar -->
+        <progress max="100" value="5"></progress>
+        <p id="inhundered">درصد پیشرفت</p>
+        <!-- progress bar -->
+        </div>
+
+  <script src="assets/script.js"></script>
+</body>
+</html>
